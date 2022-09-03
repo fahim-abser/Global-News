188be673e885e32396ca0f9f10ab97004ae8bc13
fahim-abser <fahimabser0123@gmail.com>
2022-09-03 11:07:33 +0600

    catagories update

---
+++ Global News/js/app.js
@@ -0,0 +1,19 @@
+const loadNewsCatagories = () => {
+    fetch(`https://openapi.programming-hero.com/api/news/categories`)
+        .then(res => res.json())
+        .then(data => displayCatagories(data.data.news_category))
+};
+const displayCatagories = (news) => {
+    const catagoriesSection = document.getElementById('catagories-Section');
+    news.forEach(news => {
+        console.log(news);
+        const catagoryDiv = document.createElement('div');
+        catagoryDiv.classList.add('p-4')
+        catagoryDiv.innerHTML = `
+        <p> ${news.category_name} </p>
+        `;
+        catagoriesSection.appendChild(catagoryDiv);
+        console.log(news)
+    })
+}
+loadNewsCatagories()
